1d2c8b9e84550b6bf9405f6c9bb73b2cfb4d7d9f
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2019-03-21 14:31:18 -0600

    Changed from mongodb://localhost/mongoHeadlines to mongodb://localhost/scrape

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// If deployed, use the deployed database. Otherwise use the local scrape database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape";
 
 mongoose.connect(MONGODB_URI);
 
